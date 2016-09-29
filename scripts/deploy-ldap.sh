0fe75f6c5283cd393c32f7d04e4b1a58eae483c8
Adrian G Prado <adrian.garciaprado@nrg.com>
2016-09-29 14:15:28 -0500

    Add LDAP service

---
+++ scripts/deploy-ldap.sh
@@ -0,0 +1,7 @@
+# Ansible command to deploy nts server
+
+FILE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+. ${FILE_DIR}/init.sh
+
+ansible-playbook \
+  -i ${inventory} ${PLAYBOOKS_DIR}/deploy-ldap.yml "$@"
