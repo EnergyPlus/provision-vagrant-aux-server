2f32082ae750dcbb2fefde16f9f3c95d519261ed
Adrian G Prado <adrian.garciaprado@nrg.com>
2016-10-03 12:21:57 -0500

    Ansible external roles added to init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,9 +4,10 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # folders with ansible roles
 rolesDir="${SCRIPT_DIR}/../roles"
-
+externalAnsibleRoles="/Users/agprado/Projects/NRG/AnalyticsCluster/AnsibleAllHostsRoles"
 # Ansible env var with roles paths
-export ANSIBLE_ROLES_PATH=${rolesDir}
+export ANSIBLE_ROLES_PATH=${rolesDir}:${externalAnsibleRoles}
+
 
 # Ansible config location
 export ANSIBLE_CONFIG=${SCRIPT_DIR}/ansible.cfg
