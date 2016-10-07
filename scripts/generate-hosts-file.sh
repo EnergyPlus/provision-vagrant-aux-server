c6f6eefb092d4e43cb364e2f8a295ff4a1213dd3
Adrian G Prado <adrian.garciaprado@nrg.com>
2016-10-07 16:05:07 -0500

    Move all scripts under the same directory

--- scripts/generate-hosts-file.sh
+++ scripts/generate-hosts-file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 FILE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-. ${FILE_DIR}/../init.sh
+. ${FILE_DIR}/init.sh
 
 ansible-playbook -i ${inventory} \
                     ${PLAYBOOKS_DIR}/generate-hosts-file.yml "$@"
